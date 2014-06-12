3df9aac95927daaa6c40e00432c2d7184c7e6557
Julio Cesar Sanchez Salinas <ja162001@prodigy.net.mx>
2014-06-12 21:37:50 +0200

    Versión inicial Proyecto Testigos

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Testigos::Application.config.secret_key_base = 'a629cd07f6ddb1a37bbff6b9b51d3681648532b96123dfc9851d4f26a2ef1eb2f767d5851a63499a4dcb194d8ba03fa3dddfb9424bc46a285fa841a590917cca'
